10bfb32c4c1c7491848d89ecffa859eb1eeb9f6b
GrafKenar <emukhlygin@gmail.com>
2023-03-07 18:00:09 +0300

    added tests for product page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 from .pages.main_page import *
+from .pages.login_page import *
 import time
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
